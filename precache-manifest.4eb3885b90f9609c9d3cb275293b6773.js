b196a04945c53c3d8ae1d45d1aed8ad7aed618f4
unknown <guillaumeragot92@yahoo.com>
2020-07-03 23:11:44 +0200

    Updates

---
+++ precache-manifest.4eb3885b90f9609c9d3cb275293b6773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd37e927d75e93da58a451adfba4b78",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a3c665288fa0dd0987c2",
+    "url": "/todolist/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a3c665288fa0dd0987c2",
+    "url": "/todolist/static/js/2.896ce6c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.896ce6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781df1f0d1c8eaae7510",
+    "url": "/todolist/static/js/main.bd795f34.chunk.js"
+  },
+  {
+    "revision": "658823e3cb77b75eedbc",
+    "url": "/todolist/static/js/runtime-main.587ab855.js"
+  }
+]);
